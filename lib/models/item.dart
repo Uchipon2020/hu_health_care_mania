c7dc2b5e242b7cb6424a716b9d671959455ea4a9
satoshi <uchiyama.s@timeshappy.com>
2021-08-17 15:36:45 +0900

    MAC版

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -6,21 +6,24 @@ class Item{
   String _on_the_day;  //受診日
   String _date;//更新日
   int _priority;//分類
-  String _height;
+  String _height;//身長
+  String _weight;//体重
 
   //測定値
-  dynamic heightlist = ['210.0','209.9','209.8','209.7'];
+  dynamic heightlist = ['210.0','209.8','209.8','209.7'];
+  dynamic weightlist =['100','99.9'];
 
 //コンストラクタ（初期設定）２段階。ノーマルと、ID付きコンストラクタ
-  Item(this._priority,[this._id, this._on_the_day, this._date, this._height]);
-  Item.withId(this._id, this._priority,[this._on_the_day, this._date, this._height]);
+  Item(this._priority,[this._id, this._on_the_day, this._date, this._height, this._weight]);
+  Item.withId(this._id, this._priority,[this._on_the_day, this._date, this._height, this._weight]);
 
   //getter & setter
   int get id => _id;
   String get on_the_day => _on_the_day;
   String get date => _date;
   int get priority => _priority;
-  String get height => this._height;
+  String get height => _height;
+  String get weight => _weight;
 
   set on_the_day(String newOTD){
     if (newOTD.length <= 255){
@@ -37,11 +40,16 @@ class Item{
       this._date = newDate;
     }
   }
-    set height(String newHeight){
-      if (newHeight.length <= 255){
-        this._height = newHeight;
-      }
+  set height(String newHeight){
+    if (newHeight.length <= 255){
+      this._height = newHeight;
     }
+  }
+  set weight(String newWeight){
+    if (newWeight.length <=255){
+      this._weight = newWeight;
+    }
+  }
 
 //Flutterには配列が無いので、マッピングのリストを作成
   Map<String, dynamic> toMap(){
@@ -51,6 +59,7 @@ class Item{
     map['priority'] = _priority;
     map['data'] = _date;
     map['height'] = _height;
+    map['weight'] = _weight;
     return map;
 
   }
@@ -61,5 +70,6 @@ class Item{
     this._priority = map['Priority'];
     this._date = map['date'];
     this._height = map['height'];
+    this._weight = map['weight'];
   }
 }
